7e3624f81ee567b548e0100f8fd495ca63eb947c
sandikrxyzn <92760335+sandikrxyzn@users.noreply.github.com>
2022-07-10 08:56:30 +0700

    Add files via upload

---
+++ animasi/filter/script.js
@@ -0,0 +1,4 @@
+// Who needs JS when you can make (kind of) a slider, with functionning arrow keys, in HTML/CSS for less than 300 lines total
+// Feel free to try out some colors, further below the end of the CSS file
+
+// Btw, there's a discreet button at the bottom-right corner :)
